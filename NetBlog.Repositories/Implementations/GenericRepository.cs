14143ad0c1fca53d0612626f52f3d47f4c0984c1
Abhishek Bohora <abhishek.bohora16@gmail.com>
2022-11-05 11:04:53 +0545

    generic repo

---
+++ NetBlog.Repositories/Implementations/GenericRepository.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetBlog.Repositories.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using NetBlog.Data;
+
+namespace NetBlog.Repositories.Implementations
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly DbSet<T> entities;
+
+        public GenericRepository(ApplicationDbContext context)
+        {
+            _context = context;
+            entities = context.Set<T>();
+        }
+
+        public async Task<bool> CheckExistBy(Expression<Func<T, bool>> predicate)
+        {
+            return await entities.AnyAsync(predicate);
+        }
+
+        public async Task Create(T t)
+        {
+            await entities.AddAsync(t);
+        }
+
+        public async Task Delete(Guid id)
+        {
+            var entity = await entities.FindAsync(id);
+            if (entity != null)
+            {
+                entities.Remove(entity);
+            }
+        }
+
+        public void Edit(T t)
+        {
+            entities.Update(t);
+        }
+
+        virtual public async Task<List<T>> GetAll()
+        {
+            return await entities.ToListAsync();
+        }
+
+        public async Task<List<T>> GetAllBy(Expression<Func<T, bool>> predicate)
+        {
+            return await entities.Where(predicate).ToListAsync();
+        }
+
+        public async Task<T?> GetBy(Expression<Func<T, bool>> predicate)
+        {
+            return await entities.FirstOrDefaultAsync(predicate);
+        }
+        public int TotalCount()
+        {
+            return entities.Count();
+        }
+    }
+}
