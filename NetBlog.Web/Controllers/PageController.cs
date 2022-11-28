15779ef1aeb8cc33aee29c249af9b4ae70613bea
Sailesh Rijal <saileshrijal1@gmail.com>
2022-11-28 17:01:00 +0545

    pages maintained in a page controller

---
+++ NetBlog.Web/Controllers/PageController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NetBlog.Services.Interfaces;
+using NetBlog.ViewModels;
+
+namespace NetBlog.Web.Controllers
+{
+    public class PageController : Controller
+    {
+        private readonly IPostService _postService;
+        private readonly IPageService _pageService;
+
+        public PageController(IPostService postService, IPageService pageService)
+        {
+            _postService = postService;
+            _pageService = pageService;
+        }
+
+        public async Task<IActionResult> About()
+        {
+            var vm = new BlogPageViewModel()
+            {
+                Page = await _pageService.GetPage("About"),
+                RecentPosts = await _postService.GetRecentPosts()
+            };
+            return View(vm);
+        }
+
+        public async Task<IActionResult> Contact()
+        {
+            var vm = new BlogPageViewModel()
+            {
+                Page = await _pageService.GetPage("Contact"),
+                RecentPosts = await _postService.GetRecentPosts()
+            };
+            return View(vm);
+        }
+
+        public async Task<IActionResult> PrivacyPolicy()
+        {
+            var vm = new BlogPageViewModel()
+            {
+                Page = await _pageService.GetPage("PrivacyPolicy"),
+                RecentPosts = await _postService.GetRecentPosts()
+            };
+            return View(vm);
+        }
+
+        public IActionResult NotFound()
+        {
+            return View();
+        }
+    }
+}
